1960a25981611ec74f39c94c7c04f4437f867f93
Oleksii Kupin <oleksiikupin@superghost-pc.local>
2020-02-15 19:52:23 +0200

    move pattern

--- src/client/src/containers/Board/index.js
+++ src/client/src/containers/Board/index.js
@@ -3,21 +3,22 @@ import Cell from "../../components/Cell";
 import { BoardContainer } from "./styles";
 import socketIOClient from "socket.io-client";
 
-const pattern = [
+
+const defaultPattern = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -28,23 +29,13 @@ const pattern = [
 
 const url = 'http://localhost:5000/api/items';
 const Board = () => {
-  const [data, setData] = useState('');
-  const image = pattern.flat();
+  const [pattern, setPattern] = useState(defaultPattern);
 
-  const fetchData = () => {
-    fetch(url).
-    then(res => res.json())
-    .then(data => setData(data.name))
-  }
+  const image = pattern.flat();
   
   useEffect(() => {
     const socket = socketIOClient('http://localhost:5000');
-    socket.on("village", data => 
-    
-      console.log('data = ', data)
-      // setData(data.data)
-  
-    )
+    socket.on("time", data => setPattern(data.pattern))
   }, [])
 
   return (
@@ -52,10 +43,6 @@ const Board = () => {
         {image.map(n => (
           <Cell n={n} />
         ))}
-        <h1 style={{fontSize: 30, color:'yellow'}}>
-          {data}
-        </h1>
-        <button onClick={fetchData}>Click Me</button>
       </BoardContainer>
   );
 };
