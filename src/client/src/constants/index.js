9c6377c4154887b13ea8161879e5cb5db8cbe276
Oleksii Kupin <oleksiikupin@superghost-pc.local>
2020-02-15 19:37:37 +0200

    sockets

--- src/client/src/constants/index.js
+++ src/client/src/constants/index.js
@@ -1,2 +1,2 @@
-export const CELL_SIZE = 26;
+export const CELL_SIZE = 22;
 export const CELL_MARGIN = 2;
--- src/client/src/containers/Board/index.js
+++ src/client/src/containers/Board/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import Cell from "../../components/Cell";
 import { BoardContainer } from "./styles";
+import socketIOClient from "socket.io-client";
+
 const pattern = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -9,7 +11,7 @@ const pattern = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -24,14 +26,36 @@ const pattern = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
+const url = 'http://localhost:5000/api/items';
 const Board = () => {
+  const [data, setData] = useState('');
   const image = pattern.flat();
+
+  const fetchData = () => {
+    fetch(url).
+    then(res => res.json())
+    .then(data => setData(data.name))
+  }
+  
+  useEffect(() => {
+    const socket = socketIOClient('http://localhost:5000');
+    socket.on("village", data => 
+    
+      console.log('data = ', data)
+      // setData(data.data)
+  
+    )
+  }, [])
 
   return (
       <BoardContainer>
         {image.map(n => (
           <Cell n={n} />
         ))}
+        <h1 style={{fontSize: 30, color:'yellow'}}>
+          {data}
+        </h1>
+        <button onClick={fetchData}>Click Me</button>
       </BoardContainer>
   );
 };
