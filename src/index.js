3a39c04c8be07ed32c2198c818b3dc3a0a53ac5a
Oleksii KUPIN <okupin@e1r1p1.unit.ua>
2020-03-04 20:44:13 +0200

    rebased

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './clent/index.css';
+import './client/index.css';
 import App from './client/App';
 import * as serviceWorker from './client/serviceWorker';
 
